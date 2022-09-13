bc4f13cc7505b5fdd56a780411edc83f1fb23e92
Willian Tomaz <willianlima28@hotmail.com>
2022-09-13 20:42:01 -0300

    feat: Implementando novas coberturas de testes e adicionando as funcionalidades (Seguindo o conceito TDD)

---
+++ tdd-alura-projeto/src/main/java/br/com/alura/tdd/modelo/ReajusteService.java
@@ -0,0 +1,20 @@
+package br.com.alura.tdd.modelo;
+
+import java.math.BigDecimal;
+
+public class ReajusteService {
+    public void concederReajuste(Funcionario funcionario, Desempenho desempenho) {
+        if(desempenho == Desempenho.A_DESEJAR){
+            BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.03"));
+            funcionario.reajustarSalario(reajuste);
+        }
+        else if(desempenho == Desempenho.BOM){
+            BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.15"));
+            funcionario.reajustarSalario(reajuste);
+        }
+        else if(desempenho == Desempenho.OTIMO){
+            BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.2"));
+            funcionario.reajustarSalario(reajuste);
+        }
+    }
+}
