cfca9c4b4109bac03acfae032132a3d2384f63ed
Willian Tomaz <willianlima28@hotmail.com>
2022-09-13 19:39:31 -0300

    feat: Adicionando novo teste 'bonusDeveriaSerDezPorCentoParaSalarioDeExatamente10000'

--- tdd-alura-projeto/src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
+++ tdd-alura-projeto/src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
@@ -16,16 +16,24 @@ public class BonusServiceTest {
         BonusService service = new BonusService();
         BigDecimal bonus = service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("25000")));
 
-        assertEquals(BigDecimal.ZERO, bonus);
+        assertEquals(new BigDecimal("0.00"), bonus);
     }
 
 
     @Test
-    public void bonusDeveriaSerDezPorCentoParaSalarioDeExatamente10000(){
+    public void bonusDeveriaSer10PorCentoDoSalario(){
         BonusService service = new BonusService();
         BigDecimal bonus = service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("2500")));
 
         assertEquals(new BigDecimal("250.00"), bonus);
+    }
+
+    @Test
+    public void bonusDeveriaSerDezPorCentoParaSalarioDeExatamente10000(){
+        BonusService service = new BonusService();
+        BigDecimal bonus = service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("10000")));
+
+        assertEquals(new BigDecimal("1000.00"), bonus);
     }
 
 }
